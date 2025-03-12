f30c15db650c7526c39dd287b22403a5929ab645
maxi <arora.anubhav93@gmail.com>
2025-03-12 19:55:51 +0530

    Add a test dispatcher class implementation

---
+++ app/src/test/java/com/almax/giphy/util/TestDispatcherProvider.kt
@@ -0,0 +1,20 @@
+package com.almax.giphy.util
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class TestDispatcherProvider : DispatcherProvider {
+
+    private val testDispatcher = UnconfinedTestDispatcher()
+
+    override val main: CoroutineDispatcher
+        get() = testDispatcher
+
+    override val io: CoroutineDispatcher
+        get() = testDispatcher
+
+    override val default: CoroutineDispatcher
+        get() = testDispatcher
+}
