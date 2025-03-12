5a70eeee6a54473238c5a2b0ef307c33225be5e7
maxi <arora.anubhav93@gmail.com>
2025-03-12 11:24:29 +0530

    Update application class with di
    Add the component builder implementation and dependency injection in the application class

--- app/src/main/java/com/almax/giphy/GifApplication.kt
+++ app/src/main/java/com/almax/giphy/GifApplication.kt
@@ -1,5 +1,25 @@
 package com.almax.giphy
 
 import android.app.Application
+import com.almax.giphy.di.component.ApplicationComponent
+import com.almax.giphy.di.component.DaggerApplicationComponent
+import com.almax.giphy.di.module.ApplicationModule
 
-class GifApplication : Application()
+class GifApplication : Application() {
+
+    lateinit var component: ApplicationComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        injectDependencies()
+    }
+
+    private fun injectDependencies() {
+        component = DaggerApplicationComponent
+            .builder()
+            .applicationModule(ApplicationModule(this@GifApplication))
+            .build()
+
+        component.inject(this@GifApplication)
+    }
+}
