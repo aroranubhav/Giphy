a2a319c2a4db70ebf7031da358656b4bb60161c6
maxi <arora.anubhav93@gmail.com>
2025-03-15 22:56:42 +0530

    Add constant values

--- app/src/main/java/com/almax/giphy/util/Constants.kt
+++ app/src/main/java/com/almax/giphy/util/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
 
     const val API_KEY_IDENTIFIER = "api_key"
     const val API_KEY = "NREcIOS4iOR8G0qYbYcOKZdGKSO4KF8W"
+    const val GIFS_TABLE = "gifs_table"
+    const val GIFS_DB = "gifs_database"
 }
