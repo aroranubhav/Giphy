b129a1d9e2f59c190c552755f04ea6658630db45
maxi <arora.anubhav93@gmail.com>
2025-03-15 23:05:29 +0530

    Add extension function to update imageview color

---
+++ app/src/main/java/com/almax/giphy/util/utils.kt
@@ -0,0 +1,14 @@
+package com.almax.giphy.util
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
+
+fun ImageView.tint(context: Context, @ColorRes color: Int) {
+    DrawableCompat.setTint(
+        this.drawable,
+        ContextCompat.getColor(context, color)
+    )
+}
