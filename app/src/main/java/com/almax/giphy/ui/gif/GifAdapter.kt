7d746f6ca2eacd8dea77128909b3a1544ed306ef
maxi <arora.anubhav93@gmail.com>
2025-03-15 23:01:27 +0530

    Refactor GifAdapter and add click listener for fav
    This commit refactors the code to use GifEntity in place of GifData and sets up a click listener to save and remove gifs locally

--- app/src/main/java/com/almax/giphy/ui/gif/GifAdapter.kt
+++ app/src/main/java/com/almax/giphy/ui/gif/GifAdapter.kt
@@ -1,28 +1,41 @@
 package com.almax.giphy.ui.gif
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.almax.giphy.R
-import com.almax.giphy.data.model.GifData
+import com.almax.giphy.data.local.GifEntity
 import com.almax.giphy.databinding.ItemGifBinding
+import com.almax.giphy.util.GifItemSavedListener
+import com.almax.giphy.util.tint
 import com.bumptech.glide.Glide
 
 class GifAdapter(
-    private val gifsList: ArrayList<GifData>
+    private val gifsList: ArrayList<GifEntity>
 ) : RecyclerView.Adapter<GifAdapter.GifViewHolder>() {
+
+    lateinit var itemSavedListener: GifItemSavedListener<GifEntity>
 
     inner class GifViewHolder(private val binding: ItemGifBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun onBind(gif: GifData) {
+        fun onBind(gifEntity: GifEntity) {
             binding.apply {
-                gif.apply {
+                gifEntity.apply {
                     Glide.with(root.context)
                         .asGif()
-                        .load(images.downsized.url)
+                        .load(gif.images.downsized.url)
                         .placeholder(R.drawable.ic_loading)
                         .into(ivGif)
+                    updateDrawableTintColor(isSaved, binding.root.context, ivFav)
+                }
+                ivFav.setOnClickListener {
+                    gifEntity.isSaved = !gifEntity.isSaved
+                    gifEntity.savedTimeStamp = System.currentTimeMillis().toString()
+                    updateDrawableTintColor(gifEntity.isSaved, binding.root.context, ivFav)
+                    itemSavedListener(gifEntity, gifEntity.isSaved)
                 }
             }
         }
@@ -43,7 +56,15 @@ class GifAdapter(
         holder.onBind(gifsList[position])
     }
 
-    fun setData(gifsList: List<GifData>) {
+    private fun updateDrawableTintColor(isSaved: Boolean, context: Context, view: ImageView) {
+        if (isSaved) {
+            view.tint(context, android.R.color.holo_red_light)
+        } else {
+            view.tint(context, android.R.color.black)
+        }
+    }
+
+    fun setData(gifsList: List<GifEntity>) {
         this.gifsList.clear()
         this.gifsList.addAll(gifsList)
         notifyDataSetChanged()
