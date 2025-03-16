584d418ed4e500754425f43d492c224011f2bd23
maxi <arora.anubhav93@gmail.com>
2025-03-16 12:59:55 +0530

    Add and refactor Fav viewmodel and fragment
    This commit adds logic to observe data from database and update ui, as well as pass removal action from ui to db

--- app/src/main/java/com/almax/giphy/ui/favourites/FavouritesFragment.kt
+++ app/src/main/java/com/almax/giphy/ui/favourites/FavouritesFragment.kt
@@ -1,28 +1,139 @@
 package com.almax.giphy.ui.favourites
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.almax.giphy.GifApplication
 import com.almax.giphy.R
+import com.almax.giphy.databinding.FragmentFavouritesBinding
+import com.almax.giphy.di.component.DaggerFavouritesComponent
+import com.almax.giphy.di.component.FavouritesComponent
+import com.almax.giphy.di.module.FavouritesModule
+import com.almax.giphy.ui.base.UiState
+import com.almax.giphy.ui.gif.GifAdapter
+import com.almax.giphy.util.Constants.ROFL_EMOJI
+import com.almax.giphy.util.getEmoji
+import com.almax.giphy.util.shareGif
+import com.almax.giphy.util.updateVisibility
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 class FavouritesFragment : Fragment() {
+
+    private lateinit var component: FavouritesComponent
+    private var _binding: FragmentFavouritesBinding? = null
+    private val binding
+        get() = _binding!!
+
+    @Inject
+    lateinit var viewModel: FavouritesViewModel
+
+    @Inject
+    lateinit var adapter: GifAdapter
 
     companion object {
         fun newInstance() = FavouritesFragment()
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
+    override fun onAttach(context: Context) {
+        injectDependencies(context)
+        super.onAttach(context)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_favourites, container, false)
+        _binding = FragmentFavouritesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupUi()
+    }
+
+    private fun setupUi() {
+        binding.apply {
+            rvGif.apply {
+                layoutManager = LinearLayoutManager(context)
+                adapter = this@FavouritesFragment.adapter
+                setHasFixedSize(true)
+                addItemDecoration(
+                    DividerItemDecoration(
+                        context,
+                        (layoutManager as LinearLayoutManager).orientation
+                    )
+                )
+            }
+        }
+        adapter.apply {
+            itemRemovedListener = { gif ->
+                viewModel.removeGifFromFavourites(gif)
+            }
+            itemShareListener = { url ->
+                shareGif(url)
+            }
+        }
+        observeDataAndUpdateUi()
+    }
+
+    private fun observeDataAndUpdateUi() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect { state ->
+                    when (state) {
+                        is UiState.Success -> {
+                            adapter.setData(state.data)
+                            binding.pbGif.updateVisibility(false)
+                        }
+
+                        is UiState.Error -> {
+                            Snackbar.make(
+                                binding.root,
+                                state.error,
+                                Snackbar.LENGTH_SHORT
+                            ).show()
+                            binding.pbGif.updateVisibility(false)
+                        }
+
+                        is UiState.Loading -> {
+                            binding.pbGif.updateVisibility(true)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun shareGif(url: String) {
+        val messageToShare = resources.getString(R.string.default_share_message) + " " +
+                getEmoji(ROFL_EMOJI) + "\n $url"
+        context?.let {
+            shareGif(it, messageToShare)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun injectDependencies(context: Context) {
+        component = DaggerFavouritesComponent
+            .builder()
+            .applicationComponent((context.applicationContext as GifApplication).component)
+            .favouritesModule(FavouritesModule(this@FavouritesFragment, context))
+            .build()
+        component.inject(this@FavouritesFragment)
     }
 }
--- app/src/main/java/com/almax/giphy/ui/favourites/FavouritesViewModel.kt
+++ app/src/main/java/com/almax/giphy/ui/favourites/FavouritesViewModel.kt
@@ -1,6 +1,46 @@
 package com.almax.giphy.ui.favourites
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.almax.giphy.data.local.GifEntity
+import com.almax.giphy.data.repository.FavouritesRepository
+import com.almax.giphy.ui.base.UiState
+import com.almax.giphy.util.DispatcherProvider
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
 
-class FavouritesViewModel : ViewModel() {
+class FavouritesViewModel(
+    private val repository: FavouritesRepository,
+    private val dispatcherProvider: DispatcherProvider
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow<UiState<List<GifEntity>>>(UiState.Loading)
+    val uiState: StateFlow<UiState<List<GifEntity>>>
+        get() = _uiState
+
+    init {
+        getFavouriteGifs()
+    }
+
+    private fun getFavouriteGifs() {
+        viewModelScope.launch(dispatcherProvider.main) {
+            repository.getFavouriteGifs()
+                .flowOn(dispatcherProvider.io)
+                .catch { e ->
+                    _uiState.value = UiState.Error(e.message.toString())
+                }
+                .collect {
+                    _uiState.value = UiState.Success(it)
+                }
+        }
+    }
+
+    fun removeGifFromFavourites(gifEntity: GifEntity) {
+        viewModelScope.launch(dispatcherProvider.io) {
+            repository.removeGifFromFavourites(gifEntity)
+        }
+    }
 }
