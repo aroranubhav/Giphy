b6ea3c5b423411bef8f2f39ee64571d54d5f6be8
maxi <arora.anubhav93@gmail.com>
2025-03-16 12:57:15 +0530

    Refactor gif adapter to reuse in fav fragment

--- app/src/main/java/com/almax/giphy/ui/gif/GifAdapter.kt
+++ app/src/main/java/com/almax/giphy/ui/gif/GifAdapter.kt
@@ -4,16 +4,20 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.almax.giphy.R
 import com.almax.giphy.data.local.GifEntity
 import com.almax.giphy.databinding.ItemGifBinding
+import com.almax.giphy.ui.favourites.FavouritesFragment
+import com.almax.giphy.util.GifItemRemovedListener
 import com.almax.giphy.util.GifItemSavedListener
 import com.almax.giphy.util.GifShareListener
 import com.almax.giphy.util.tint
 import com.bumptech.glide.Glide
 
 class GifAdapter(
+    private val fragment: Fragment,
     private val gifsList: ArrayList<GifEntity>
 ) : RecyclerView.Adapter<GifAdapter.GifViewHolder>() {
 
@@ -21,9 +25,10 @@ class GifAdapter(
 
     lateinit var itemShareListener: GifShareListener<String>
 
+    lateinit var itemRemovedListener: GifItemRemovedListener<GifEntity>
+
     inner class GifViewHolder(private val binding: ItemGifBinding) :
         RecyclerView.ViewHolder(binding.root) {
-
         fun onBind(gifEntity: GifEntity) {
             binding.apply {
                 gifEntity.apply {
@@ -34,11 +39,19 @@ class GifAdapter(
                         .into(ivGif)
                     updateDrawableTintColor(isSaved, binding.root.context, ivFav)
                 }
-                ivFav.setOnClickListener {
-                    gifEntity.isSaved = !gifEntity.isSaved
-                    gifEntity.savedTimeStamp = System.currentTimeMillis().toString()
-                    updateDrawableTintColor(gifEntity.isSaved, binding.root.context, ivFav)
-                    itemSavedListener(gifEntity, gifEntity.isSaved)
+                if (fragment is GifFragment) {
+                    ivFav.setOnClickListener {
+                        gifEntity.isSaved = !gifEntity.isSaved
+                        gifEntity.savedTimeStamp = System.currentTimeMillis().toString()
+                        updateDrawableTintColor(gifEntity.isSaved, binding.root.context, ivFav)
+                        itemSavedListener(gifEntity, gifEntity.isSaved)
+                    }
+                } else if (fragment is FavouritesFragment) {
+                    ivFav.setOnClickListener {
+                        gifsList.remove(gifEntity)
+                        itemRemovedListener(gifEntity)
+                        notifyDataSetChanged()
+                    }
                 }
                 ivShare.setOnClickListener {
                     itemShareListener(gifEntity.gif.url)
