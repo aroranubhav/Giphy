989663f8eace51c08b20e88a9a873efddb5b3164
maxi <arora.anubhav93@gmail.com>
2025-03-16 13:00:20 +0530

    Add extension method and reusable util method

--- app/src/main/java/com/almax/giphy/util/utils.kt
+++ app/src/main/java/com/almax/giphy/util/utils.kt
@@ -1,6 +1,10 @@
 package com.almax.giphy.util
 
 import android.content.Context
+import android.content.Intent
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.widget.ImageView
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
@@ -15,4 +19,22 @@ fun ImageView.tint(context: Context, @ColorRes color: Int) {
 
 fun getEmoji(unicode: Int): String {
     return String(Character.toChars(unicode))
+}
+
+fun View.updateVisibility(state: Boolean) {
+    if (state) this.visibility = VISIBLE else this.visibility = GONE
+}
+
+fun shareGif(context: Context, message: String) {
+    val sendIntent: Intent = Intent().apply {
+        action = Intent.ACTION_SEND
+        putExtra(
+            Intent.EXTRA_TEXT,
+            message
+        )
+        type = "text/plain"
+    }
+
+    val shareIntent = Intent.createChooser(sendIntent, null)
+    context.startActivity(shareIntent)
 }
