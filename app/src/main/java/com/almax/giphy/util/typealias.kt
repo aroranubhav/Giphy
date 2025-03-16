c4664a175b450cc9d30b0ca501bf79cff1168884
maxi <arora.anubhav93@gmail.com>
2025-03-16 12:58:41 +0530

    Add Gif removal method definition

--- app/src/main/java/com/almax/giphy/util/typealias.kt
+++ app/src/main/java/com/almax/giphy/util/typealias.kt
@@ -3,3 +3,5 @@ package com.almax.giphy.util
 typealias GifItemSavedListener<T> = (data: T, isSaved: Boolean) -> Unit
 
 typealias GifShareListener<T> = (data: T) -> Unit
+
+typealias GifItemRemovedListener<T> = (data: T) -> Unit
